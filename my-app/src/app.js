487b4bbde89c3e107f1aa5ddab10ec024b3787e8
lenatormina <lena.tormina2001@mail.ru>
2024-10-28 22:21:18 +0300

    Настроить проект

---
+++ my-app/src/app.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Div = styled.div`
+	text-align: center;
+`;
+
+export const App = () => {
+	return (
+		<Div>
+			<i class="fa fa-calculator" aria-hidden="true"></i>
+			<div>123</div>
+		</Div>
+	);
+};
