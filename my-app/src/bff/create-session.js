47d25fd8ef621ca4a9bbd03a91b8ad55689b605a
lenatormina <lena.tormina2001@mail.ru>
2024-11-01 22:15:59 +0300

    Создать BFF

---
+++ my-app/src/bff/create-session.js
@@ -0,0 +1,29 @@
+import { removeComment } from './session';
+import { ROLE } from '../constans';
+
+export const cteateSession = (roleId) => {
+	let session = {
+		logout() {
+			Object.keys(session).forEach((key) => {
+				delete session[key];
+			});
+		},
+	};
+
+	switch (roleId) {
+		case ROLE.ADMIN: {
+			session.removeComment = removeComment;
+			break;
+		}
+		case ROLE.MODERATOR: {
+			session.removeComment = removeComment;
+			break;
+		}
+		case ROLE.READER: {
+			break;
+		}
+		default:
+	}
+
+	return session;
+};
