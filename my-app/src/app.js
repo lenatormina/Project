0695e97b59ea94db7d1b0afd2d1dd42d0cd05934
lenatormina <lena.tormina2001@mail.ru>
2024-11-06 22:22:40 +0300

    Реализовать шапку

--- my-app/src/app.js
+++ my-app/src/app.js
@@ -1,5 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
+import { Header } from './components';
 import styled from 'styled-components';
+
+const AppColumn = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	width: 1000px;
+	min-height: 100%;
+	margin: 0 auto;
+	background: #fff;
+`;
 
 const Content = styled.div`
 	padding: 120px 0;
@@ -9,12 +20,11 @@ const H2 = styled.h2`
 	text-align: center;
 `;
 
-const Header = () => <div>Шапка</div>;
 const Footer = () => <div>Футер</div>;
 
 export const App = () => {
 	return (
-		<>
+		<AppColumn>
 			<Header />
 			<Content>
 				<H2>Контент страницы</H2>
@@ -23,12 +33,12 @@ export const App = () => {
 					<Route path="/login" element={<div>Авторизация</div>} />
 					<Route path="/register" element={<div>Регистрация</div>} />
 					<Route path="/users" element={<div>Пользователи</div>} />
-					<Route path="/post" element={<div>Новая статья</div>} />
-					<Route path="/post/:postId" element={<div>Статья</div>} />
+					<Route path="/material" element={<div>Новый материал</div>} />
+					<Route path="/material/:materialId" element={<div>Материал</div>} />
 					<Route path="*" element={<div>Ошибка</div>} />
 				</Routes>
 			</Content>
 			<Footer />
-		</>
+		</AppColumn>
 	);
 };
---
+++ my-app/src/components/header/header.js
@@ -0,0 +1,32 @@
+import { ControlPanel, Logo } from './components';
+import styled from 'styled-components';
+
+const Discription = styled.div`
+	font-style: italic;
+	justify-self: center;
+	align-self: center;
+	font-size: 20px;
+`;
+
+const HeaderConteiner = ({ className }) => (
+	<header className={className}>
+		<Logo />
+		<Discription>
+			В простых и <br />
+			понятных схемах
+		</Discription>
+		<ControlPanel />
+	</header>
+);
+
+export const Header = styled(HeaderConteiner)`
+	display: flex;
+	justify-content: space-between;
+	position: fixed;
+	top: 0;
+	width: 1000px;
+	height: 120px;
+	padding: 20px 40px;
+	background-color: #fff;
+	box-shadow: 0px -2px 17px #000;
+`;
