4e2003698877c832dce669b5c0e3d5f4bcd1b84d
Elena <e.tormina@vinteo.ru>
2024-12-10 16:31:28 +0300

    Доработка вида

--- my-app/src/components/header/components/control-panel/control-panel.js
+++ my-app/src/components/header/components/control-panel/control-panel.js
@@ -50,12 +50,12 @@ const ControlPanelContainer = ({ className }) => {
 				)}
 			</RigthAlign>
 			<RigthAlign>
-				<Icon id="fa-backward" margin="5px 0 0 0" onClick={() => navigate(-1)} />
+				<Icon id="fa-backward" margin="10px 0 0 0" onClick={() => navigate(-1)} />
 				<Link to="/material">
-					<Icon id="fa-file-text-o" margin="5px 0 0 15px"/>
+					<Icon id="fa-file-text-o" margin="10px 0 0 15px" />
 				</Link>
 				<Link to="/users">
-					<Icon id="fa-users" margin="5px 0 0 15px"/>
+					<Icon id="fa-users" margin="10px 0 0 13px" />
 				</Link>
 			</RigthAlign>
 		</div>
